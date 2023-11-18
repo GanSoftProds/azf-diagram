ff1678b84e7a0ff6ef425260f185b2a0573ed132
evirusky <evasabater17@gmail.com>
2023-11-18 16:06:29 +0100

    added random id

--- src/readFunctionJSON.js
+++ src/readFunctionJSON.js
@@ -1,5 +1,16 @@
 const fs = require('fs-extra');
 
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
 
 async function readFunctionJSON(file) {
   let bindings = [];
@@ -7,12 +18,19 @@ async function readFunctionJSON(file) {
   try {
 
     const jsonString = await fs.readJson(file);
+    let name;
 
     for (let i = 0; i < jsonString.bindings.length; i++) {
+      if (!jsonString.bindings[i].connection){
+        name = makeid(5);
+      }
+      else{
+        name = jsonString.bindigs[i].connection;
+      }
       bindings.push({
         type: jsonString.bindings[i].type,
         direction: jsonString.bindings[i].direction,
-        resource_name: jsonString.bindings[i].connection
+        resource_name: name
       });
     }
 
