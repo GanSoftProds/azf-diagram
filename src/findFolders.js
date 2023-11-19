3f87006fe7d8b545e445e49d44469ab7d2c1b54b
evirusky <evasabater17@gmail.com>
2023-11-19 12:06:20 +0100

    added directories

--- src/findFolders.js
+++ src/findFolders.js
@@ -1,19 +1,24 @@
 const fs = require('fs');
-const pathL = require('path');
+const path = require('path');
 
-let wd = "/home/juan/Escritorio/opensourcejam202311/azure_function_example/"
+let readFunctionJSON = require("./readFunctionJSON");
+
+let wd =  process.argv.slice(2)[0];
+
+
 function getDirectories(path) {
   return fs.readdirSync(path).filter(function (file) {
     return fs.statSync(path + '/' + file).isDirectory();
   })
 }
 
-function getValidFunctionsPath(dirList) {
+function getValidFunctionsPath(dirList, wd) {
 
   for (const dir of dirList) {
 
-    if (fs.existsSync(dir + "/function.json")) {
+    if (fs.existsSync(wd + dir + "/function.json")) {
       console.log("Azure function encontrada: " + dir)
+      readFunctionJSON(wd + dir + "/function.json")
     }
   }
 
@@ -21,7 +26,8 @@ function getValidFunctionsPath(dirList) {
 }
 
 let paths = getDirectories(wd)
-console.log(paths);
+console.log(paths); 
 
-getValidFunctionsPath(paths);
-console.log("\nCurrent directory filenames:");
+getValidFunctionsPath(paths, wd);
+
+
